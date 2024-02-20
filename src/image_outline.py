b3203d754859829c302499f7e0aada2c3e1b9fa4
saltbread1 <takenokonosato913@icloud.com>
2024-02-21 01:34:18 +0900

    o

--- src/image_outline.py
+++ src/image_outline.py
@@ -1,26 +1,27 @@
 import os
 import xml.etree.ElementTree as ET
 import svg.path
-
-IMG_NAME = 'sample.svg'
-img_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'images', IMG_NAME)
-
-tree = ET.parse(img_path)
-root = tree.getroot()
-
-paths = root.findall('.//{http://www.w3.org/2000/svg}path')
-
-for path in paths:
-    d = path.get('d')
-    svg_path = svg.path.parse_path(d)
-    for attr in svg_path:
-        if isinstance(attr, svg.path.Move):
-            print(attr.start)
-        elif isinstance(attr, svg.path.Line):
-            print(attr)
-        elif isinstance(attr, svg.path.CubicBezier):
-            print(attr)
-        else:
-            print(attr)
+import curve
 
 
+def get_image_outlines(filename):
+    filepath = os.path.join(os.path.dirname(__file__), '..', 'resources', 'images', filename)
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+
+    paths = root.findall('.//{http://www.w3.org/2000/svg}path')
+    curves = []
+    for path in paths:
+        d = path.get('d')
+        svg_path = svg.path.parse_path(d)
+        for attr in svg_path:
+            if isinstance(attr, svg.path.Move):
+                pass
+            elif isinstance(attr, svg.path.Line):
+                curves.append(curve.Line(attr.start, attr.end))
+            elif isinstance(attr, svg.path.CubicBezier):
+                curves.append(curve.CubicBezier(attr.start, attr.control1, attr.control2, attr.end))
+            else:
+                print(f'Unknown attribute: {attr}')
+
+    return curves
