d36ff2f713b8d22b6766cb00b4c4aad0a37b1a5b
saltbread1 <takenokonosato913@icloud.com>
2024-02-21 01:21:44 +0900

    curve

--- src/image_outline.py
+++ src/image_outline.py
@@ -1,13 +1,26 @@
-from os import path
+import os
 import xml.etree.ElementTree as ET
 import svg.path
 
 IMG_NAME = 'sample.svg'
-img_path = path.join(path.dirname(__file__), '..', 'resources', 'images', IMG_NAME)
-print(img_path)
+img_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'images', IMG_NAME)
 
 tree = ET.parse(img_path)
 root = tree.getroot()
 
-print(ET.tostring(root))
+paths = root.findall('.//{http://www.w3.org/2000/svg}path')
+
+for path in paths:
+    d = path.get('d')
+    svg_path = svg.path.parse_path(d)
+    for attr in svg_path:
+        if isinstance(attr, svg.path.Move):
+            print(attr.start)
+        elif isinstance(attr, svg.path.Line):
+            print(attr)
+        elif isinstance(attr, svg.path.CubicBezier):
+            print(attr)
+        else:
+            print(attr)
+
 
