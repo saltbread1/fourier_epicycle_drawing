39116f2c1fc131105c7e76f3591149f8126435ab
saltbread1 <takenokonosato913@icloud.com>
2024-02-22 01:33:53 +0900

    graph wip

--- src/epicycle.py
+++ src/epicycle.py
@@ -18,6 +18,7 @@ class Epicycle:
     def initialize(self, filename, plot_space):
         self.curves = self.create_curve(filename, 8)
         print(len(self.curves))
+        self.create_eulerian_graph()
         # self.plots = np.array([point for curve in self.curves for point in curve.get_points(plot_space)])
         # print(len(self.plots))
         # self.sort_plots(plot_space)
@@ -33,36 +34,38 @@ class Epicycle:
         #         curves.remove(curve)
         #         print(curve.start, curve.end)
 
-        # for curve in curves:
-        #     # set of start and end
-        #     min_dists = [np.inf, np.inf]
-        #     other_points = [None, None]
-        #
-        #     for other in curves:
-        #         if curve.__eq__(other):
-        #             continue
-        #         for anchor in [other.start, other.end]:
-        #             tmp_dists = [abs(anchor - curve.start), abs(anchor - curve.end)]
-        #             # min_dists = [min(tmp_dists[i], min_dists[i]) for i in range(2)]
-        #             if tmp_dists[0] < min_dists[0] and tmp_dists[1] > dist_thresh:
-        #                 min_dists[0] = tmp_dists[0]
-        #                 other_points[0] = anchor
-        #             if tmp_dists[1] < min_dists[1] and tmp_dists[0] > dist_thresh:
-        #                 min_dists[1] = tmp_dists[1]
-        #                 other_points[1] = anchor
-        #     if min_dists[0] < dist_thresh:
-        #         curve.start = other_points[0]
-        #     if min_dists[1] < dist_thresh:
-        #         curve.end = other_points[1]
-        #
-        # for curve in curves:
-        #     if curve.start == curve.end:
-        #         curves.remove(curve)
-        #         print(1)
+        for this_curve in curves:
+            # set of start and end
+            min_dists = [np.inf, np.inf]
+            other_points = [None, None]
+
+            for other_curve in curves:
+                if this_curve.__eq__(other_curve):
+                    continue
+                for other_anchor in [other_curve.start, other_curve.end]:
+                    tmp_dists = [abs(other_anchor - this_curve.start), abs(other_anchor - this_curve.end)]
+                    # min_dists = [min(tmp_dists[i], min_dists[i]) for i in range(2)]
+                    # avoid curve.start = curve.end
+                    if tmp_dists[0] < min_dists[0] and tmp_dists[1] > dist_thresh:
+                        min_dists[0] = tmp_dists[0]
+                        other_points[0] = other_anchor
+                    if tmp_dists[1] < min_dists[1] and tmp_dists[0] > dist_thresh:
+                        min_dists[1] = tmp_dists[1]
+                        other_points[1] = other_anchor
+            if min_dists[0] < dist_thresh:
+                this_curve.start = other_points[0]
+            if min_dists[1] < dist_thresh:
+                this_curve.end = other_points[1]
+
+        for this_curve in curves:
+            if this_curve.start == this_curve.end:
+                curves.remove(this_curve)
+                print(1)
 
         return curves
 
     def create_eulerian_graph(self):
+        # create graph based on a curve set
         graph = nx.Graph()
         for curve in self.curves:
             start = self.complex2string(curve.start)
@@ -70,23 +73,31 @@ class Epicycle:
             graph.add_node(start)
             graph.add_node(end)
             graph.add_edge(start, end, weight=curve.get_length())
-        
 
-    def sort_plots(self, plot_space):
-        # create graph
-        graph = nx.Graph()
-        # graph.add_nodes_from(self.point2str(plot) for plot in self.plots)
-        # for plot in self.plots:
-        #     for curve in self.curves:
-        #         dist, other = self.get_min_dist_point(plot, curve.get_points(plot_space))
-        #         if dist < plot_space * 2.:
-        #             graph.add_edge(self.point2str(plot), self.point2str(other))
-        # print(1)
-        # print(chinese_postman(graph))
+        # make it a single connected graph
+        for component in nx.connected_components(graph):
+            if len(list(nx.connected_components(graph))) == 1:
+                break
+            this_points = [self.string2complex(x) for x in component]
+            min_dist = np.inf
+            min_points = None
+            for this_point in this_points:
+                other_points = [self.string2complex(x) for x in graph if x not in component]
+                tmp_dist, other_point = self.get_min_dist_point(this_point, other_points)
+                if tmp_dist < min_dist:
+                    min_dist = tmp_dist
+                    min_points = [this_point, other_point]
+            graph.add_edge(self.complex2string(min_points[0]), self.complex2string(min_points[1]))
+
+        # make it an eulerian graph
+        new_graph = nx.MultiGraph()
+        new_graph.add_nodes_from(graph.nodes())
+        new_graph.add_edges_from(graph.edges())
+
 
     @staticmethod
     def complex2string(c):
-        return f'{c.real}, {c.imag}'
+        return f'{c.real},{c.imag}'
 
     @staticmethod
     def string2complex(str):
@@ -94,16 +105,17 @@ class Epicycle:
         return complex(float(x), float(y))
 
     @staticmethod
-    def get_min_dist_point(point, points):
+    def get_min_dist_point(point, all_points):
         min_dist = np.inf
-        ret_point = None
-        for other in points:
+        other_point = None
+        for other in all_points:
             if other == point:
                 continue
             tmp_dist = abs(point - other)
             if tmp_dist < min_dist:
-                ret_point = other
-        return min_dist, ret_point
+                min_dist = tmp_dist
+                other_point = other
+        return min_dist, other_point
 
     def draw_outline(self):
         py5.push()
@@ -114,16 +126,16 @@ class Epicycle:
         py5.stroke_weight(0.4)
         for curve in self.curves:
             curve.draw()
+        # start points
+        py5.stroke_weight(4)
+        py5.stroke(0xffff0000)
+        for curve in self.curves:
+            py5.point(curve.start.real, curve.start.imag)
         # end points
         py5.stroke_weight(2)
         py5.stroke(0xff0000ff)
         for curve in self.curves:
             py5.point(curve.end.real, curve.end.imag)
-        # start points
-        py5.stroke_weight(3)
-        py5.stroke(0xffff0000)
-        for curve in self.curves:
-            py5.point(curve.start.real, curve.start.imag)
         # all plots
         # for plot in self.plots:
         #     py5.point(plot.real, plot.imag)
